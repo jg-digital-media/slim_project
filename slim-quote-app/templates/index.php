4bbde55b98e28c52fb2a76421ada1fe858635f75
Jonathan Grieve <mail@jonniegrieve.co.uk>
2026-05-15 12:57:50 +0100

    add link to browser icon

--- slim-quote-app/templates/index.php
+++ slim-quote-app/templates/index.php
@@ -16,6 +16,9 @@ $basepath = '';
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..90display=swap" rel="stylesheet">
 
+    <!-- Icons -->
+    <link rel="icon" href="<?= $basepath ?>/../favicon.ico">
+
     <!-- Stylesheets -->
     <link rel="stylesheet" type="text/css" href="<?= $basepath ?>/../style.css">
 
